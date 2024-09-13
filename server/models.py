506dc172f5c41141357a613563e062088f3084b8
Chori <yidodeveloper@gmail.com>
2024-09-13 01:52:40 +0000

    Fix: 모델에서 일부 항목 길이 제한 설정

---
+++ server/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Sentence(models.Model):
+    student_id = models.CharField(max_length=9, default='000000000')
+    category_id = models.CharField(max_length=1000, default='카테고리')
+    question = models.CharField(max_length=1000, default='질문')
+    answer1 = models.TextField(max_length=None, default='답변1')
+    answer2 = models.TextField(max_length=None, default='답변2')
+    comment = models.TextField(max_length=None, default='진단')
+    grade = models.CharField(max_length=2, default='등급')
