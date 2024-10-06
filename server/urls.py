9fe1a369ebe6661179c42cf11ee734ee0a400f88
Chori <yidodeveloper@gmail.com>
2024-10-07 00:17:17 +0900

    ✨ 전체 문항 전송 기능 구현

--- server/urls.py
+++ server/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from server.views import Sentence
+from server.views import SentenceView
 
 # APIView로 구현한 CURD에 대한 URL
 urlpatterns = [
-    path('result/', Sentence.as_view())
+    path('result/', SentenceView.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- server/views.py
+++ server/views.py
@@ -3,15 +3,17 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from server.serializers import SentenceSerializer
+from server.models import Sentence
 
 # Create your views here.
 
-class Sentence(APIView):
-    def post(self, request):
+class SentenceView(APIView):
+    def get(self, request):
 
-        data = request.data.copy()
+        # 데이터베이스에서 모든 Sentence 객체 가져오기
+        sentences = Sentence.objects.all()
     
-        serializer = SentenceSerializer(data=data)
-        if serializer.is_valid():
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        # 여러 객체를 직렬화할 경우, many=True 옵션을 추가
+        serializer = SentenceSerializer(sentences, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
