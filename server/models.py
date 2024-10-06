bc2a5f8299a6ae8d917984903a3bbc1f824229b0
Chori <yidodeveloper@gmail.com>
2024-10-06 10:42:40 +0900

    🗃️ 데이터 모델 마이그레이션

--- server/models.py
+++ server/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
 class Sentence(models.Model):
-    student_id = models.CharField(max_length=9, default='000000000')
-    category_id = models.CharField(max_length=1000, default='카테고리')
+    category = models.CharField(max_length=1000, default='카테고리')
     question = models.CharField(max_length=1000, default='질문')
+
+"""
+    student_id = models.CharField(max_length=9, default='000000000')
     answer1 = models.TextField(max_length=None, default='답변1')
     answer2 = models.TextField(max_length=None, default='답변2')
     comment = models.TextField(max_length=None, default='진단')
     grade = models.CharField(max_length=2, default='등급')
+"""
