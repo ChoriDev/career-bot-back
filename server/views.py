00132dd1cfafd505c9df10f3bbe3e9311b2b9853
Chori <yidodeveloper@gmail.com>
2024-12-17 15:23:54 +0900

    🚸 학생이 작성한 문장을 코멘트와 같이 전송

--- server/views.py
+++ server/views.py
@@ -8,6 +8,15 @@ from server.models import Question
 from server.models import Answer
 from server.request_func import send_post_to_fastapi
 
+def create_sentence(question_content, answer1, answer2):
+    # 질문의 "_"를 각각의 answer로 대체하여 문장을 생성
+    if answer2:
+        # 두 개의 답변이 있을 경우
+        return question_content.replace('_', answer1, 1).replace('_', answer2)  # 첫 번째 밑줄에 answer1, 두 번째 밑줄에 answer2
+    else:
+        # 하나의 답변만 있을 경우
+        return question_content.replace('_', answer1)  # 밑줄을 answer1로 대체
+
 class QuestionView(APIView):
     def get(self, request):
 
@@ -45,10 +54,10 @@ class AnswerView(APIView):
         answer2 = request.data.get('answer2', None)  # answer2가 없으면 None으로 설정
 
         # 문장 생성
-        sentence = self.create_sentence(question.content, answer1, answer2)
+        sentence = create_sentence(question.content, answer1, answer2)
 
         # 문장 생성
-        sentence = self.create_sentence(question.content, answer1, answer2)
+        sentence = create_sentence(question.content, answer1, answer2)
 
         # Bert 서버로 데이터 전송
         bert_response = send_post_to_fastapi(sentence, url='http://career-bot-bert:8001/grade')
@@ -80,15 +89,6 @@ class AnswerView(APIView):
         # Django에서 처리된 데이터를 반환
         # return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(status=status.HTTP_200_OK)
-    
-    def create_sentence(self, question_content, answer1, answer2):
-        # 질문의 "_"를 각각의 answer로 대체하여 문장을 생성
-        if answer2:
-            # 두 개의 답변이 있을 경우
-            return question_content.replace('_', answer1, 1).replace('_', answer2)  # 첫 번째 밑줄에 answer1, 두 번째 밑줄에 answer2
-        else:
-            # 하나의 답변만 있을 경우
-            return question_content.replace('_', answer1)  # 밑줄을 answer1로 대체
 
 class ResultView(APIView):
     def get(self, request, student_id):
@@ -99,32 +99,47 @@ class ResultView(APIView):
             return Response({'error': '해당 학생의 답변이 없습니다.'}, status=status.HTTP_404_NOT_FOUND)
 
         grade_table = {
-            '자기이해 및 긍정적 자아상': [[], []],
-            '대인관계 및 의사소통 역량': [[], []],
-            '교육기회의 탐색': [[], []],
-            '진로의사결정능력': [[], []],
-            '진로 설계와 준비': [[], []],
-            '직업정보의 탐색': [[], []],
-            '건강한 직업의식': [[], []],
-            '직업세계 이해': [[], []]
+            '자기이해 및 긍정적 자아상': [],
+            '대인관계 및 의사소통 역량': [],
+            '교육기회의 탐색': [],
+            '진로의사결정능력': [],
+            '진로 설계와 준비': [],
+            '직업정보의 탐색': [],
+            '건강한 직업의식': [],
+            '직업세계 이해': []
         }
-        
+
         for answer in answers:
             category = answer.question_id.category
             if category in grade_table:
-                # 3번째 문장까지만 자르기 위한 작업
-                sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', answer.comment)  # 문장 분할
-                shortened_comment = " ".join(sentences[:3])  # 3번째 문장까지 가져옴
-                grade_table[category][0].append(shortened_comment)  # 잘라낸 코멘트를 추가
-                grade_table[category][1].append(answer.grade)
+                # 질문과 답변을 결합한 문장 생성
+                question_content = answer.question_id.content
+                answer1 = answer.answer1
+                answer2 = answer.answer2
+                student_sentence = create_sentence(question_content, answer1, answer2)
 
+                # 3번째 문장까지만 자르기 위한 작업
+                sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', answer.comment)
+                shortened_comment = " ".join(sentences[:3])
+
+                # 결과에 추가
+                grade_table[category].append({
+                    'student_sentence': student_sentence,
+                    'comment': shortened_comment,
+                    'grade': answer.grade
+                })
+
+        # 각 카테고리별 평균 점수 계산 및 결과 생성
         result = {}
-        for category, grades in grade_table.items():
-            comments = grades[0]
-            if grades[1]:  # 빈 리스트가 아닌 경우에만 평균 계산
-                grade = sum(grades[1]) / len(grades[1])
-            else:  # 데이터가 없는 경우 0으로 설정
-                grade = 0
-            result[category] = [comments, grade]
+        for category, entries in grade_table.items():
+            comments = [entry['comment'] for entry in entries]
+            student_sentences = [entry['student_sentence'] for entry in entries]
+            grades = [entry['grade'] for entry in entries]
+            average_grade = sum(grades) / len(grades) if grades else 0
+            result[category] = {
+                'student_sentences': student_sentences,
+                'comments': comments,
+                'average_grade': average_grade
+            }
 
         return Response(result, status=status.HTTP_200_OK)
